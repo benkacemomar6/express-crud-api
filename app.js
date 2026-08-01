0d8e6fa3a36a0965252923566d2e2c7a8368331f
omar <benkacemomar6@gmail.com>
2026-08-01 19:17:19 +0100

    Build Express CRUD API with routers and middleware

---
+++ app.js
@@ -0,0 +1,18 @@
+const esxpres=require('express');
+const app=esxpres();
+app.use(esxpres.json());
+const logger=require("./mideleware/logger")
+const userRouter=require("./rout/user-root")
+app.use(logger);
+
+
+
+app.use("/users",userRouter);
+app.get("/",(req,res)=>{
+
+    res.send('home page')
+})
+app.listen(3000,()=>{
+    console.log("server runinng");
+})
+
